43584c28098a0b6d562f7b920cd0e7ba4cf655ca
Tlanetzi Chavez <tlane@MacBook-Air-de-Tlanetzi.local>
2020-11-24 17:03:13 -0700

    Clase Producto

---
+++ Producto.h
@@ -0,0 +1,41 @@
+#ifndef Producto_h
+#define Producto_h
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Producto{
+    private:
+
+    double precio;
+    string tamaño;
+
+    Producto();
+    Producto(double nPrecio, string nTamaño);
+    void setPrecio(double precio);
+    void setTamaño(string tamaño);
+
+    double getPrecio();
+    string getTamaño();
+
+};
+
+Producto::Producto(){
+    precio = 0;
+    tamaño = "-";
+}
+
+Producto::Producto(double nPrecio, string nTamaño){
+    precio = nPrecio;
+    tamaño = nTamaño;
+}
+
+double Producto::getPrecio(){
+     return precio;
+}
+string Producto::getTamaño(){
+    return tamaño;
+}
+
+#endif /* Producto_h */
